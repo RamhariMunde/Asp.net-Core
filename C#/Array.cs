ff9f966142ab3fdd75d41750d7c00ec29b1c1c7a
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2024-07-15 07:44:20 +0530

    2D Array sum & Subtraction

--- C#/Array.cs
+++ C#/Array.cs
@@ -71,3 +71,67 @@ namespace paper1
     }
 }
 }
+
+/*
+// this is for subtraction of two Array 
+
+  int[,]A = new int[2, 2];   
+  int[,]B = new int[2, 2];
+  int[,]C = new int[2, 2];
+
+  Console.WriteLine("Enter the 4 Elements of Array A: ");
+  for (int i = 0; i < 2; i++) 
+  {
+    for(int j = 0; j < 2; j++)
+      {
+          A[i,j] =Convert.ToInt32(Console.ReadLine());
+      }
+  }
+
+  Console.WriteLine("Enter the 4 Elements of Array B: ");
+  for(int i=0; i<2; i++)
+  {
+      for(int j=0; j<2; j++)
+      {
+          B[i,j] = Convert.ToInt32(Console.ReadLine());
+      }
+  }
+
+ for(int i= 0; i<2;i++)
+  {
+      for(int j=0 ; j<2 ; j++)
+      {
+          C[i,j] = A[i,j]-B[i,j];
+      }
+  }
+  Console.WriteLine("this is Array of A: ");
+  for(int i=0;i<2; i++)
+  {
+      for(int j=0;j<2; j++)
+      {
+          Console.Write(A[i,j]+"\t");
+      }
+      Console.WriteLine();
+  }
+
+  Console.WriteLine("this is Array of B :");
+  for(int i=0;i<2; i++)
+  {
+      for(int j=0 ; j<2; j++)
+      {
+          Console.Write(B[i,j]+"\t");
+      }
+      Console.WriteLine();
+  }
+
+  Console.WriteLine("this is a substraction of A and B Array : ");
+  for(int i=0; i< 2; i++)
+  {
+      for(int j=0;j<2; j++)
+      {
+          Console.Write(C[i,j]+"\t");
+      }
+      Console.WriteLine();
+  }
+  Console.ReadLine();
+*/
