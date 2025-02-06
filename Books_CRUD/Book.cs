b69967327f617177fbe2841d89874a066a3798ef
Ramhari Munde <89252634+RamhariMunde@users.noreply.github.com>
2025-02-06 23:48:17 +0530

    Add files via upload

---
+++ Books_CRUD/Book.cs
@@ -0,0 +1,9 @@
+﻿namespace Book_App.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string? title { get; set; }
+        public string? Author { get; set; }
+    }
+}
